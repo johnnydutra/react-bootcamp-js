346481f7ed5c37c87c29487b1f3b1ed3fb80727c
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-02 13:51:00 -0300

    39: javascript logic in components

--- src/index.js
+++ src/index.js
@@ -73,6 +73,14 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if () alert(`We're currently open!`);
+  // else alert(`Sorry, we're closed`);
+
   return (
     <footer>
       It's {new Date().toLocaleTimeString()}. We're currently open.{' '}
