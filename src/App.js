ad7d1f16ec15ec5862b2964115362ebf29a67d71
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-03 08:01:22 -0300

    57: building a steps component

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const messages = [
+  'Time trial',
+  '7-week training cycle',
+  'Half-marathon full send',
+];
+
+export default function App() {
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: '#7950f2', color: '#ffff' }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#ffff' }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
