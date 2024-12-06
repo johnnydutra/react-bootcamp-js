c6b45983113842abf56cfcd9fd2a657ac3f1b971
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-06 15:25:41 -0300

    116: creating the stars

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    {/* <StarRating maxRating={10} />
+    <StarRating /> */}
   </React.StrictMode>
 );
