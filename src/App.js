bc227bbf6b135bf627e3c3ce1921f3040d5ec2a9
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-11 08:27:28 -0300

    145: using an async function

--- src/App.js
+++ src/App.js
@@ -55,11 +55,17 @@ const KEY = 'b3e6d3d9';
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+  const query = 'interstellar';
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
   }, []);
 
   return (
